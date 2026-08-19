6e3eec112a7f93b0544cff0958507b892e10eb3a
surajdhakad <102307386+surajdhakad@users.noreply.github.com>
2026-08-19 13:23:11 +0530

    Update Api.js

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,35 +1,26 @@
 import axios from "axios";
 
 const Api = axios.create({
-
-    baseURL: "http://localhost:5000/api",
-
-    headers: {
-        "Content-Type": "application/json",
-    }
-
+  baseURL: "http://localhost:5000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-// Automatically Token Send
-
+// Automatically send token
 Api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
 
-    (config) => {
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
 
-        const token = localStorage.getItem("token");
-
-        if (token) {
-
-            config.headers.Authorization = `Bearer ${token}`;
-
-        }
-
-        return config;
-
-    },
-
-    (error) => Promise.reject(error)
-
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export default Api;
